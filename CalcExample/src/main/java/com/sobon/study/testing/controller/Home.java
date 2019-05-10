14ebff747e86ec9d733be0f9d1c078edee129258
Wojciech Soboń <wojciech.sobon@microsoft.wsei.edu.pl>
2019-05-10 20:15:20 +0200

    Prosty kalkulator

---
+++ CalcExample/src/main/java/com/sobon/study/testing/controller/Home.java
@@ -0,0 +1,37 @@
+package com.sobon.study.testing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Home {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String greeting(@RequestParam(value="Liczba1", required=false)  String number1,
+                           @RequestParam(value = "Liczba2", required = false) String number2,
+                           @RequestParam(value = "mathType", required = false) String mathType,
+                           Model model) {
+        if (number1 != null && number2 != null && mathType != null) {
+            switch (mathType) {
+                case "Dodaj" :
+                    model.addAttribute("result", Double.parseDouble(number1) + Double.parseDouble(number2));
+                    break;
+                case "Odejmij" :
+                    model.addAttribute("result", Double.parseDouble(number1) * Double.parseDouble(number2));
+                    break;
+                case "Podziel" :
+                    model.addAttribute("result", Double.parseDouble(number1) / Double.parseDouble(number2));
+                    break;
+                default:
+                    throw new RuntimeException("Unsupported operation");
+
+            }
+        }
+        return "index";
+    }
+
+
+}
